48012e3785e1179ec8d0685503dece9ed741ca16
Matthew Chick <chikmatthew@gmail.com>
2017-09-12 12:43:48 +1000

    Setup view for posts, categories and users

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// php artisan make:auth to create the following routes
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
---
+++ resources/views/admin/admin/posts/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeremychik
+ * Date: 12/9/2017
+ * Time: 12:36 PM
+ */
