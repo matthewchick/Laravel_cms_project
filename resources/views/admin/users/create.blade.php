9ffc562e4b695a737ad21d5659c56bda5b024e95
Matthew Chick <chikmatthew@gmail.com>
2017-09-15 12:13:31 +1000

    Add Form inside the create.blade.php but have an error

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,7 +1,26 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jeremychik
- * Date: 12/9/2017
- * Time: 12:39 PM
- */
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Create Users</h1>
+    {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store']) !!}
+    <div class="form-group">
+        {!! Form::label('name', 'Name:') !!}
+        {!! Form::text('name', null, ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('email', 'Email:') !!}
+        {!! Form::email('email', null, ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('role_id', 'Role:') !!}
+        {!! Form::text('role_id', null, ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('status', 'Status:') !!}
+        {!! Form::text('status', null, ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::submit('Create User',['class'=>'btn btn-primary']) !!}
+    </div>
+    {!! Form::close() !!}
+@endsection
