0e28847ef4718915e035624c26e5c4eb8ca53355
Matthew Chick <chikmatthew@gmail.com>
2017-09-19 11:35:17 +1000

    Create a relationship between user and photo

--- app/User.php
+++ app/User.php
@@ -4,28 +4,40 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password', 'role_id', 'photo_id','is_active','',
-    ];
+class User extends Authenticatable {
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name',
+		'email',
+		'password',
+		'role_id',
+		'photo_id',
+		'is_active',
+		'',
+	];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
 
-    // set relation with Role
-    public function role() {
-        return $this->belongsTo('App\Role');
-    }
+	// set relation with Role
+	public function role() {
+		return $this->belongsTo( 'App\Role' );
+	}
+
+
+	public function photo() {
+		return $this->belongsTo( 'App\Photo' );
+	}
 }
+
