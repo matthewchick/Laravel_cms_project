e641935219a6eaa32816bc22c3741bf43af38211
Matthew Chick <chikmatthew@gmail.com>
2017-09-22 12:17:05 +1000

    display the posts before mass assignment inside posts.php

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+	protected $fillable = [   //mass assignment to be consistent with DB
+		'category_id',
+		'photo_id',
+		'title',
+		'body'
+	];
 }
