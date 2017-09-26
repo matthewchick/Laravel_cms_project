d1b4b175b4c4480ed37cd6f8dd149f9e992937b1
Matthew Chick <chikmatthew@gmail.com>
2017-09-26 10:40:24 +1000

    Add index and edit for categories

--- app/Http/Controllers/AdminCategoriesController.php
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -15,7 +16,8 @@ class AdminCategoriesController extends Controller
      */
     public function index()
     {
-        return view('admin.categories.index');
+    	$categories = Category::all();
+        return view('admin.categories.index', compact('categories'));
     }
 
     /**
